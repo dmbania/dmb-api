ba13601cdb3e6648cdf9a52484b78cdfd0855aaf
dbania <davebania@rocketmortgage.com>
2023-10-20 14:22:20 -0400

    Add configuration module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 import { DmbController } from './dmb/dmb.controller';
+import { Module } from '@nestjs/common';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController, DmbController],
   providers: [AppService],
 })
